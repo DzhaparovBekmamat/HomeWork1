0a695d09f83ce9100dc2f96e95ecb5315e6c3ea8
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-20 19:18:55 +0600

    Update Messi.java

--- src/Messi.java
+++ src/Messi.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Messi {
+public class Messi extends Football_player{
+    public Messi(String name, int yearOfBirth, double height, String bodyType, String football_club, Position position) {
+        super(name, yearOfBirth, height, bodyType, football_club, position);
+    }
 }
