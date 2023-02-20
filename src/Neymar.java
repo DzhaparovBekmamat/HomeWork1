6d7df5bf9b733682d334dab4a40d6b69561c9d52
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-20 19:19:17 +0600

    Update Neymar.java

--- src/Neymar.java
+++ src/Neymar.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Neymar {
+public final class Neymar extends Football_player {
+    public Neymar(String name, int yearOfBirth, double height, String bodyType, String football_club, Position position) {
+        super(name, yearOfBirth, height, bodyType, football_club, position);
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo()+"\nNEYMAR IS THE BEST FOOTBALL PLAYER IN BRAZIL";
+    }
 }
+
+
