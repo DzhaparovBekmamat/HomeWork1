24c552c55b077d0fa546c08813e1ad12e8efd5b8
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-20 19:20:03 +0600

    Update Position.java

--- src/Position.java
+++ src/Position.java
@@ -1,3 +1,14 @@
-public enum Position {
-    RIGHT_WINGER, LEFT_WINGER, CENTRAL_FORWARD
+public class Ronaldo extends Football_player{
+    public Ronaldo(String name, int yearOfBirth, double height, String bodyType, String football_club, Position position) {
+        super(name, yearOfBirth, height, bodyType, football_club, position);
+    }
+    public void cost(){
+        System.out.println("СТОИМОСТЬ ФУТБОЛИСТА ДОСТИАГЕТ ДО 180 МЛН ЕВРО !");
+    }
+    public final void cost(int cost){
+        if (cost<=0){
+            System.out.println("ФУТБОЛИСТ ИГРАЕТ В ЛЮБИТЕЛЬСКОЙ ЛИГЕ !");
+        }
+        System.out.println("СТОИМОСТЬ ФУТБОЛИСТА НА ДАННЫЙ МОМЕНТ = "+cost+" МЛН ЕВРО");
+    }
 }
